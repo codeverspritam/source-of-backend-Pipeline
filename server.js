65861fe3eb9c8fa485ece00b028554241f7cbfce
Pritam <codeverspritam@users.noreply.github.com>
2026-09-25 11:32:00 +0530

    about cretaed

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
-
