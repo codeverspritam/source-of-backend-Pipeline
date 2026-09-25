b8333966c048a46e1b5af7de079cce08733d2951
Pritam <codeverspritam@users.noreply.github.com>
2026-09-25 14:51:29 +0530

    info

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.get("/about", (req, res) => {
   res.send("About page");
 });
 
+app.get("/info",(req, res)=>{
+    res.send("Information page")
+})
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
